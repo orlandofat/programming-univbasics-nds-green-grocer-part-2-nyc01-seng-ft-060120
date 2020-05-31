4cc90f0af9688e0b5380e9eec11099fea4217d4f
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 21:32:23 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -30,10 +30,6 @@ cart
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
-  
   counter = 0 
   while counter < cart.length
     if cart[counter][:clearance]
@@ -45,13 +41,5 @@ cart
 end
 
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  
 end
