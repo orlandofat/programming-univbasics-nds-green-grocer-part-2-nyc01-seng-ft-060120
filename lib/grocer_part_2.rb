491dfe83bf875830b0fb038837375f8c7c037885
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 21:51:07 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -48,7 +48,11 @@ def checkout(cart, coupons)
   total = 0 
   counter = 0 
   while counter < final_cart.length
-     total +=
-  
+     total += final_cart[counter][:price] * final_cart[counter][:count]
+     counter += 1
+   end
+     if total > 100
+       total -= (total * 0.10)
 end
+total
 end
