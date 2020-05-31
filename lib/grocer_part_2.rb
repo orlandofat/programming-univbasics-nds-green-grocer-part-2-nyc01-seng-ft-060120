6f7753ea12f4b9a1ccdc32c5d7fbe8a0587e32e3
Orlando Challenger <orlandofat@gmail.com>
2020-05-31 21:27:26 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -36,7 +36,8 @@ def apply_clearance(cart)
   
   counter = 0 
   while counter < cart.length
-    if 
+    if cart[counter][:clearance]
+      cart[counter][:price] = (cart[counter][:price] - (cart[counter][:price] * 0.20)).round(2)
   counter += 1 
 end
 end
